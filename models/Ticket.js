c4ac72977be7d50c9f512997d9d93b893d46e37c
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-05-29 16:20:16 +0600

    improve[ticket-model]: add validation for ticket creation

--- models/Ticket.js
+++ models/Ticket.js
@@ -2,10 +2,14 @@ const shortId  = require("shortid");
 class Ticket{
     /**
      *  Constructor function to create a ticket
-     * @param {String} username 
-     * @param {Number} price 
+     * @param {String} username  ticket's buyer name
+     * @param {Number} price  ticket price
      */
     constructor(username, price) {
+        // Validation 
+        if (!(username && price)) throw new Error("username and price both filed are required");
+        if (typeof username != "string") throw new Error("username must be a type of string");
+        if (typeof price != "number") throw new Error("price must be a type of number");
         this.id = shortId.generate();
         this.username = username;
         this.price = price;
